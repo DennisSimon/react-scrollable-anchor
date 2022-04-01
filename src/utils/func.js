bffeb898d2ce1f24f53d9f03e71b95127ec5393c
Dennis Simon <dennism.simon94@gmail.com>
2022-04-01 17:22:10 +0200

    gather arguments from rest instead of using arguments keyword

--- src/utils/func.js
+++ src/utils/func.js
@@ -1,19 +1,18 @@
 export const debounce = (func, wait, immediate) => {
-  let timeout
-  return () => {
-    const context = this
-    const args = arguments
+  let timeout;
+  return (...args) => {
+    const context = this;
     const later = () => {
-      timeout = null
+      timeout = null;
       if (!immediate) {
-        func.apply(context, args)
+        func.apply(context, args);
       }
-    }
-    const callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
     if (callNow) {
-      func.apply(context, args)
+      func.apply(context, args);
     }
-  }
-}
+  };
+};
